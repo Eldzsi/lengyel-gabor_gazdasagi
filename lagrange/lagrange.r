9966e73f18400e1e1596cecaea95808628618777
Lengyel Gábor <lengyelgabor23@gmail.com>
2025-03-18 18:50:36 +0100

    Add Lagrange interpolation function

---
+++ lagrange/lagrange.r
@@ -0,0 +1,23 @@
+x = c(1, 2, 3, 4)
+y = c(5, 2.3, 3.2, 7)
+x0 = 5
+
+lagrange_interpolation = function(x, y, x0) {
+    n = length(x)
+    L = numeric(n)
+  
+    for (i in 1:n) {
+        L[i] = 1
+        for (j in 1:n) {
+        if (i != j) {
+            L[i] = L[i] * (x0 - x[j]) / (x[i] - x[j])
+        }
+        }
+    }
+  
+    y0 = sum(y * L)
+    return(y0)
+}
+
+result = lagrange_interpolation(x, y, x0)
+print(result)
