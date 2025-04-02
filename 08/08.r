db99ddc5b236ff0879544c85832f0deacbfdade1
Lengyel Gábor <lengyelgabor23@gmail.com>
2025-04-02 10:53:08 +0200

    Upload normal distribution progra

---
+++ 08/08.r
@@ -0,0 +1,32 @@
+k = c(20, 45, 101, 132, 224, 190, 156, 87, 41, 4)
+c_values = c(-2.0, -1.5, -1.0, -0.5, 0, 0.5, 1.0, 1.5, 2.0)
+
+main = function(k) {
+    N = 0
+    for (i in 1:length(k)) {
+        N = N + k[i]
+    }
+
+    cat("N =", N, "\n")
+
+    s = 0
+    for (i in 1:length(k)) {
+        p = 0
+        if (i == 1) {
+            p = pnorm(c_values[i])
+        } else if (i == length(k)) {
+            p = 1 - pnorm(c_values[i - 1])
+        } else {
+            p = pnorm(c_values[i]) - pnorm(c_values[i - 1])
+        }
+
+        cat(sprintf("p%d = %.4f\n", i, p))
+
+        s = s + (k[i] - p * N)^2 / (p * N) 
+    }
+
+    cat("sum =", s, "\n")
+
+}
+
+main(k)
