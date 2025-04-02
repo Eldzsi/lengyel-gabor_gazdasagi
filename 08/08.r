42e48e8b8faf19d3f67af31f050d268e0382a15f
Lengyel Gábor <lengyelgabor23@gmail.com>
2025-04-02 11:00:11 +0200

    Update program

--- 08/08.r
+++ 08/08.r
@@ -1,7 +1,19 @@
 k = c(20, 45, 101, 132, 224, 190, 156, 87, 41, 4)
 c_values = c(-2.0, -1.5, -1.0, -0.5, 0, 0.5, 1.0, 1.5, 2.0)
 
-main = function(k) {
+normal_distribution_test = function() {
+    if (length(k) == 0) {
+        stop("Error: Invalid k vecto length!")
+    }
+
+    if (length(c_values) == 0) {
+        stop("Error: Invalid c_values vector length!")
+    }
+
+    if (length(c_values)+1 != length(k)) {
+        stop("Error: Invalid vector lengths!")
+    }
+
     N = 0
     for (i in 1:length(k)) {
         N = N + k[i]
@@ -25,8 +37,8 @@ main = function(k) {
         khi = khi + (k[i] - p * N)^2 / (p * N) 
     }
 
-    cat("khi =", khi, "\n")
-
+    return(khi)
 }
 
-main(k)
+khi = normal_distribution_test()
+cat("khi =", khi, "\n")
